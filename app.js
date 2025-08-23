115c94556f18282ebd8c47b330128bebc194aa08
iburah <ibsailesi2@gmail.com>
2025-08-23 13:23:45 +0200

    In-memory storage
    Created an  in-memory storage using an array.
    Imported path module and defined public folder for static files.
    Tried passing the posts storage into the index ejs.

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 //We are importing libraries using require method
 const express = require('express');
+const path = require('path'); //imported path module to handle file paths
 //an app istance of the express module
 const app = express();
 const PORT = 3000;
+// -----In-Memory storage setup
+let posts = [
+    {id:1, title: "Welcome to Our Blog", content: 'First Post'},
+];
 
+//--MIDDLEWARE-- 
 //initializing a set up of ejs template to successfully render the html files
 app.set('view engine', 'ejs');
-
+//initializing the code to serve static files contained in folder public
+app.use(express.static(path.join(__dirname, public)));
 //the views folder has an index.ejs for homepage, it will list all posts
+
+
 app.get('/', (req, res)=>{
-    res.render('index');    //we will render the index ejs file
+    res.render('index', {posts});    //we will render the index ejs file
 });
 
 //starting the server on declared port
