dac15e3cd582b02c2383e2575df4294cdab68f89
iburah <ibsailesi2@gmail.com>
2025-08-23 13:53:59 +0200

    The create route
    Added route for creating and handling posts.
    Fixed an error on public path by changing it from public to 'public'.

--- app.js
+++ app.js
@@ -10,18 +10,39 @@ let posts = [
 ];
 
 //--MIDDLEWARE-- 
-//initializing a set up of ejs template to successfully render the html files
-app.set('view engine', 'ejs');
 //initializing the code to serve static files contained in folder public
-app.use(express.static(path.join(__dirname, public)));
+app.use(express.static(path.join(__dirname, 'public')));
 //initinalizing middleware for bodyparser to handle complex forms
 app.use(bodyParser.urlencoded({extended:true}));
+//initializing a set up of ejs template to successfully render the html files
+app.set('view engine', 'ejs');
+// Telling Express where the EJS files are located
+app.set('views', path.join(__dirname, 'views'));
 
+//--ROUTES--
 
 //the views folder has an index.ejs for homepage, it will list all posts
 app.get('/', (req, res)=>{
     res.render('index', {posts});    //we will render the index ejs file
 });
+
+//route to display the form for creating posts
+app.get('/create', (req, res)=>{
+    res.render('create', {error: null, old: {}}); //passing an object where no err on first form show, and old for no input previously
+});
+
+//route to save new posts created
+app.post('/create', (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.render('create', { error: 'Title and content required', old: req.body });
+  }
+  const newPost = { id: posts.length + 1, title, content };
+  posts.push(newPost);
+  res.redirect('/');
+});
+
+
 
 //starting the server on declared port
 app.listen(PORT, () =>{
