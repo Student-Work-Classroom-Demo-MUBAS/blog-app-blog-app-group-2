5a3a337ba34d29766393f9fcb0017978eb102e8e
iburah <ibsailesi2@gmail.com>
2025-08-23 12:42:47 +0200

    Update app.js
    I have created a simple server using express. I have tested it on port 3000, and successfully running.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+//We are importing libraries using require method
+const express = require('express');
+//an app istance of the express module
+const app = express();
+const PORT = 3000;
 
+
+//starting the server on declared port
+app.listen(PORT, () =>{
+    console.log('Server running at http://localhost' + PORT);
+});
