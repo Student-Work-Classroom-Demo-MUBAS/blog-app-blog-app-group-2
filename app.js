c213cceeb16a7119f1b5eee25dbabcab6b533d5c
iburah <ibsailesi2@gmail.com>
2025-08-23 14:14:07 +0200

    Routes creation continues
    Created edit route.
    Created deleting route.
    Created posting route.
    All these routes are based on id fetching. Will keep on editing the app as we start on fetching the ejs files.

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const path = require('path'); //imported path module to handle file paths
 const bodyParser = require('body-parser'); // module to handle form submissions
+
 const app = express(); //an app istance of the express module
-const PORT = 3000;
+const PORT = 3003;
+
 // -----In-Memory storage setup
 let posts = [
     {id:1, title: "Welcome to Our Blog", content: 'First Post'},
@@ -41,6 +43,36 @@ app.post('/create', (req, res) => {
   posts.push(newPost);
   res.redirect('/');
 });
+//route to show a post as full content: fetches a existing blog post by id
+app.get('/post/:id', (req, res) => {
+  const post = posts.find(p => p.id === parseInt(req.params.id));   //looks for the post with the matching id in your posts array
+  if (!post) return res.status(404).send('Post not found');     //send errorif no match is available
+  res.render('post', { post });
+});
+
+//route to  edit post form
+app.get('/edit/:id', (req, res) => {
+  const post = posts.find(p => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).send('Post not found');
+  res.render('edit', { post, error: null });
+});
+
+//route to handle edited form submission
+app.post('/edit/:id', (req, res) => {
+  const post = posts.find(p => p.id === parseInt(req.params.id));
+  post.title = req.body.title;
+  post.content = req.body.content;
+  res.redirect('/post/' + post.id);
+});
+
+//route to delete post
+app.post('/delete/:id', (req, res) => {
+  const index = posts.findIndex(p => p.id === parseInt(req.params.id));
+  posts.splice(index, 1);
+  res.redirect('/');
+});
+
+
 
 
 
