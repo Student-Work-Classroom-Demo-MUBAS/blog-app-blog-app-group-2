5190132c8d9c96f53ff5deac7b13a319e0493304
iburah <ibsailesi2@gmail.com>
2025-08-23 13:03:15 +0200

    Created Homepage route
    I have a route to render the homepages through ejs file index. The ejs has been set as a view engine to successfully render html files. I have also installed nodemon module for auto-starting the server on changes.
    The homepage paragraph successfully shows on the page on route '/'.

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//initializing a set up of ejs template to successfully render the html files
+app.set('view engine', 'ejs');
+
+//the views folder has an index.ejs for homepage, it will list all posts
+app.get('/', (req, res)=>{
+    res.render('index');    //we will render the index ejs file
+});
 
 //starting the server on declared port
 app.listen(PORT, () =>{
-    console.log('Server running at http://localhost' + PORT);
+    console.log('Server running at http://localhost:' + PORT);
 });
