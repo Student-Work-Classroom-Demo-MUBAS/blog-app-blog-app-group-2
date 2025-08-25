d4d881305e0d2b5e19c72bbb9bee759f31dd145a
Tatyana C <165555578+Nachya7@users.noreply.github.com>
2025-08-25 19:20:14 +0200

    Update script. js
    Implementing  theme toggle button to switch between dark and light mode, saves user preference in local storage

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -22,7 +22,46 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+//Get button element that will toggle dark or light mode
+const modeToggle = document.getElementById("modeToggle");
 
+//Get body element, where the dark or light class will be applied
+const body = document.body;
+
+// Check if user has a saved theme in local storage
+if(localStorage.getItem("theme") === "dark") {
+  
+  //if saved theme is dark then add darkmode class to body
+  body.classList.add("dark-mode");
+
+  // update toggle button icon/text to indicate light mode switching
+  modeToggle.innerHTML = '<i class="fa fa-sun"></i> Light Mode';
+} else {
+  //If no saved theme or light mode is theme, add light mode class to body 
+  body.classList.add("light-mode");
+}
+
+// Add click event listener to toggle button
+modeToggle.addEventListener("click", () => {
+  //Toggle between darkmode and light mode classes on body
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+
+  //Check which mode is active after toggling
+  if(body.classList.contains("dark-mode")) {
+    //if dark mode is active:
+    //-Change button icon/text to indicate clicking will switch to light theme
+    //-Save dark in local storage to remember choice
+    modeToggle.innerHTML = '<i class="fa fa-sun"></i> Light Mode';
+    localStorage.setItem("theme", "dark");
+  } else {
+    //If light mode is active:
+    //-Change button icon/text to indicate clicking will switch to dark theme
+    //-Save light in local storage to remember choice
+    modeToggle.innerHTML = '<i class="fa fa-moon"></i> Dark Mode';
+    localStorage.setItem("theme", "light");
+  }
+});
 
 //script to move back up wehn clicked
 // Get the button
